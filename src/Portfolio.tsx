b7a678ff1266f489dfe11c3b1804e635ceaf5d6e
Mutur <komorebi.amm@gmail.com>
2024-02-12 12:03:32 +0100

    install tailwindCSS

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './main.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Portfolio() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="p-20">
+      <div className="w-96 h-48 bg-blue-500 drop-shadow-lg rounded flex justify-center items-center">
+        <p className="font-extrabold text-2xl text-white">Test</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default Portfolio
