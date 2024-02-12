b44c0a7511a8b0523ee19848e9d4f379faa3556d
Mutur <komorebi.amm@gmail.com>
2024-02-12 18:54:55 +0100

    header

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,12 +1,11 @@
+import Header from './components/Header';
 import './main.css'
 
 function Portfolio() {
   return (
-    <div className="p-20">
-      <div className="w-96 h-48 bg-blue-500 drop-shadow-lg rounded flex justify-center items-center">
-        <p className="font-extrabold text-2xl text-white">Test</p>
-      </div>
-    </div>
+    <>
+      <Header/>
+    </>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,83 @@
+import { ReactNode, useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+
+const navAnchors = [
+  {
+    title: 'Home',
+    anchor: '/',
+  },
+  {
+    title: 'About',
+    anchor: '/',
+  },
+  {
+    title: 'Portfolio',
+    anchor: '/',
+  },
+  {
+    title: 'Contact',
+    anchor: '/',
+  },
+]
+
+function Header(): ReactNode {
+  const [open, setOpen] = useState(false)
+  const handleMenu = () => {
+    setOpen((prev) => !prev)
+  }
+  return (
+    <nav className='sticky'>
+      <div className="mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <p className="text-black font-body text-xl font-bold uppercase">
+              amm
+            </p>
+          </div>
+          {/* navlinks */}
+          <div className="hidden md:block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              {navAnchors.map((anchor, index) => (
+                <a
+                  key={index}
+                  className="text-gray-500 transition-all duration-500 hover:text-black px-2 text-lg font-body"
+                  href={anchor.anchor}
+                >
+                  {anchor.title}
+                </a>
+              ))}
+            </div>
+          </div>
+          {/* hamburger button */}
+          <div className="-mr-2 flex md:hidden">
+            <button
+              type="button"
+              onClick={handleMenu}
+              className="inline-flex items-center justify-center p-2 rounded-md text-black hover:text-black focus:outline-none"
+            >
+              <span className="sr-only">Open</span>
+              {open == true ? <FaTimes /> : <FaBars />}
+            </button>
+          </div>
+        </div>
+        {/* mobile-menu */}
+        {open ? (
+          <div className="md:hidden">
+            <div className="ox-2 pt-2 pb-3 space-y-1 sm:px-3">
+              {navAnchors.map((anchor) => (
+                <a
+                  className="text-gray-900 hover:text-black hover:bg-white px-3 py-2 font-medium font-body flex justify-end"
+                  href={anchor.anchor}
+                >
+                  {anchor.title}
+                </a>
+              ))}
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </nav>
+  )
+}
+
+export default Header
